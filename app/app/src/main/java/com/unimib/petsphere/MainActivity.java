d1d383632104bea502e8940863058feb432ccefd
Alessia Mazzeo <a.mazzeo11@campus.unimib.it>
2025-01-27 00:04:59 +0100

    aggiunta bottom toolbar in main activity,da implementare navigation

--- app/app/src/main/java/com/unimib/petsphere/MainActivity.java
+++ app/app/src/main/java/com/unimib/petsphere/MainActivity.java
@@ -7,18 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+       
     }
 }
