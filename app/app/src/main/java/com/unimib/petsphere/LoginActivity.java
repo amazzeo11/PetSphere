b73ab9b8cbd6c62a60f10cd3f569dccffcdca18c
Sara Angeretti <sara.a98@live.it>
2025-01-26 17:17:22 +0100

    implementato controllo validità (solo lunghezza) della password inserita nella schermata login

--- app/app/src/main/java/com/unimib/petsphere/LoginActivity.java
+++ app/app/src/main/java/com/unimib/petsphere/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.unimib.petsphere;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +10,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = LoginActivity.class.getName();
+
+    private TextInputEditText editTextEmail, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        editTextEmail = findViewById(R.id.textInputEmail);
+        editTextPassword = findViewById(R.id.textInputPassword);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(view -> {
+            if (isEmailOk(editTextEmail.getText().toString()) && isPasswordOk(editTextPassword.getText().toString())) {
+                Log.d(TAG, "Launch new activity.");
+            }
+            else {
+                Log.e(TAG, "Error, Ig");
+            }
+        });
+    }
+
+    private boolean isEmailOk(String email) {
+        return true;
+    }
+
+    private boolean isPasswordOk(String password) {
+        return (password.length() > 7);
     }
 }
--- app/app/src/main/java/com/unimib/petsphere/MainActivity.java
+++ app/app/src/main/java/com/unimib/petsphere/MainActivity.java
@@ -1,6 +1,7 @@
 package com.unimib.petsphere;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +11,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "Uh-oh, something went wrong...");
     }
 }
