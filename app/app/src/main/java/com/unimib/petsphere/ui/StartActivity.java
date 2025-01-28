623fd42546b451cf694988ccd3ccfdcdba2358d8
Sara Angeretti <sara.a98@live.it>
2025-01-28 13:07:39 +0100

    ripristinato linear layout per fragment login e signup

--- app/app/src/main/java/com/unimib/petsphere/ui/StartActivity.java
+++ app/app/src/main/java/com/unimib/petsphere/ui/StartActivity.java
@@ -15,7 +15,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_start);
     }
 
 }
