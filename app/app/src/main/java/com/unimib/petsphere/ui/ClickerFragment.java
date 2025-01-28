0ad34e8a8acd09c02b9956f7b5731f32d22408c3
Alessia Mazzeo <a.mazzeo11@campus.unimib.it>
2025-01-28 22:57:05 +0100

    implementata pagina clicker funzionante, suono da sostituire, aggiunto layout card preview pet

--- app/app/src/main/java/com/unimib/petsphere/ui/ClickerFragment.java
+++ app/app/src/main/java/com/unimib/petsphere/ui/ClickerFragment.java
@@ -1,5 +1,6 @@
 package com.unimib.petsphere.ui;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,20 +11,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+
 import com.unimib.petsphere.R;
 
-
 public class ClickerFragment extends Fragment {
-    
+
+    private MediaPlayer mediaPlayer;
 
     public ClickerFragment() {
         // Required empty public constructor
     }
 
     public static ClickerFragment newInstance() {
-        ClickerFragment fragment = new ClickerFragment();
-
-        return fragment;
+        return new ClickerFragment();
     }
 
     @Override
@@ -40,5 +41,29 @@ public class ClickerFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Button button = view.findViewById(R.id.clicker_button);
+
+        button.setOnClickListener(v -> {
+            if (mediaPlayer != null) {
+                mediaPlayer.release();
+            }
+
+            mediaPlayer = MediaPlayer.create(getContext(), R.raw.provaclicker);
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(mp -> {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            });
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
